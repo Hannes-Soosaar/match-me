96022c03d588cc4ba32dfa8cd46964fa1fb8d4e2
hannes <hannes.study@gmail.com>
2024-11-27 10:53:05 +0200

    additional model notes

--- backend/models/user-match.go
+++ backend/models/user-match.go
@@ -6,6 +6,12 @@ import "time"
 // how to page
 // how many scores should we calculate
 // when should the score be calculated
+// Compare interest  each interest in-common is 1 point 
+// Compare multiple choice questions each some questions will give a point if one is eg. if the answer is not a match
+// Compare locations if not in specific range the score modified if x0  // only filter if is shown. ?? do we need to even do this in the scores tab?
+// Compare Slider scores each match is 1 point each difference is  1 -0.25 points and if one answer is opposite the score is  there are 4 steps
+//
+
 
 type UsersMatches struct {
 	ID        int     `json:"id"`
