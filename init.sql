f2a6f8e0fbec425caefbf82de6dd9ffb4b38d0dc
hannes <hannes.study@gmail.com>
2024-12-02 20:22:58 +0200

    Added tables for profile creation

--- init.sql
+++ init.sql
@@ -37,10 +37,85 @@ CREATE TABLE connections (
  'I am a gamer'
  );*/
 
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(255) NOT NULL
+
+);
+
+INSERT INTO categories (category)
+VALUES 
+("Genre"), -- 1
+("Play Style"), --2
+("Platform"), --3
+("Communication"), --4
+("Goals"), --5
+("Session"), --6
+("Vibe"), --7
+("Language"), --8
+;
+
 CREATE TABLE interests (
     id SERIAL PRIMARY KEY,
-    interest_name VARCHAR(50) NOT NULL,
-),
+    categoryID VARCHAR(255) NOT NULL
+    interest VARCHAR(255) NOT NULL,
+);
+
+-- Can choose at least 1 max 3 from category
+INSERT INTO interests(categoryId,interest)
+VALUES 
+(1,"RPG"),
+(1,"FPS"),
+(1,"MMO"),
+(1,"MOBA"),
+(1,"Turn Based"),
+(1,"Simulation"),
+(1,"RPG"),
+(1,"A-RPG"),
+(1,"Survival"),
+(1,"PVP"),
+(1,"PVE"),
+(2,"Casual"),
+(2,"Competitive"),
+(2,"Relaxed"),
+(2,"AFK"),
+(2,"Enthusiast"),
+(2,"Leeroy Jenkins"),
+(3,"X-box"),
+(3,"Switch"),
+(3,"PC"),
+(3,"Playstation"),
+(3,"Mobile"),
+(4,"voice chat "),
+(4,"text chat "),
+(4,"in-game chat"),
+(4,"Discord"),
+(4,"What ever, works"),
+(5,"Socialize"),
+(5,"Ranking"),
+(5,"Challenge"),
+(5,"Hang-out"),
+(5,"For laughs"),
+(6,"I got an hour to play"),
+(6,"I need at least a few hours."),
+(6,"I can go all day, every day."),
+(7,"Competitive"),
+(7,"Chill"),
+(7,"High-energy"),
+(7,"Laid-back"),
+(7,"What-ever"),
+(7,"Banter ahead"),
+(7,"Black humor"),
+(8,"Estonian"),
+(8,"English"),
+(8,"Texan"),
+(8,"German"),
+(8,"French"),
+(8,"Russian"),
+(8,"Chinese"),
+;
+
+
 
 /* The JSNOB is  a new type not used before needs testing*/
 
@@ -55,4 +130,6 @@ CREATE TABLE multiple_choice_questions (
 --     '["Option A", "Option B", "Option C", "Option D"]'::jsonb,
 --     2
 -- );
+
+
 
