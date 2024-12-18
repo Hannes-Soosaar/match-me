7b08217039a936ddbf5e3ef23c2af17f86a708a4
karl-hendrikkahn <karlhendrikkahn@gmail.com>
2024-12-18 22:20:42 +0200

    continued work on profile page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -6,23 +6,22 @@ import './Profile.css';
 const Profile = () => {
 
     const [username, setUsername] = useState('');
-    const [headerText, setHeaderText] = useState('');
+    const [aboutMe, setAboutMe] = useState('');
+    const [usernameText, setUsernameText] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        alert('This feature is WIP')
+        alert(`This feature is WIP\nUsername: ${username}\nAbout Me: ${aboutMe}`);
     }
 
 
 
     useEffect(() => {
         const profileNotExist = localStorage.getItem('profileExists') === 'doesNotExist';
-        if (profileNotExist) {
-            console.log(profileNotExist)
-            setHeaderText("Please complete your profile");
+        if (!profileNotExist) {
+            setUsernameText("Change your username");
         } else {
-            console.log(profileNotExist)
-            setHeaderText("Change your profile information if needed");
+            setUsernameText("Choose your username")
         }
     }, [])
 
@@ -35,23 +34,27 @@ const Profile = () => {
                         setting a profile picture should be possible. If nothing is set, a placeholder image is used instead.
                         "About me" section is optional.
                         */}
-                <div className="profile-header">
-                    <div className="profile-text">
-                        {headerText}
-                    </div>
-                </div>
                 <div className='inputs'>
-                    <div className='profile-text'>Choose your username</div>
+                    <div className='profile-text'>{usernameText}</div>
                     <div className='input'>
                         <input
                             type='username'
                             placeholder='Username'
+                            maxLength="20"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
                             required />
                     </div>
-                    <div className='profile-text'>What is your...</div>
-                    <div className='profile-text'>Favorite game genres?</div>
+                    <div className='profile-text'>Write something about yourself</div>
+                    <div className='input-textarea'>
+                        <textarea
+                            placeholder='About me'
+                            maxLength="500"
+                            value={aboutMe}
+                            onChange={(e) => setAboutMe(e.target.value)}
+                            required />
+                    </div>
+                    <div className='profile-text'>Upload a profile picture*</div>
                 </div>
                 <div className='submit-container'>
                     <div
@@ -68,47 +71,5 @@ const Profile = () => {
     )
 
 };
-
-// Render the dashboard if the response is successful
-
-
-
-// if (userData.id === 0 || userData.username === '' || userData.profile_picture === '') {
-//     return (
-//         <div style={{ textAlign: 'center' }}>
-//             <div className='profile-container'>
-//                 {/* 
-//                 TODO force user to fill out their profile before they have access to anything else.
-//                 Creating a username and answering biographical information questions is mandatory,
-//                 setting a profile picture should be possible. If nothing is set, a placeholder image is used instead.
-//                 "About me" section is optional.
-//                 */}
-//                 <div className='inputs'>
-//                     <div className='profile-text'>Choose your username</div>
-//                     <div className='input'>
-//                         <input
-//                             type='username'
-//                             placeholder='Username'
-//                             value={username}
-//                             onChange={(e) => setUsername(e.target.value)}
-//                             required />
-//                     </div>
-//                     <div className='profile-text'>What is your...</div>
-//                     <div className='profile-text'>Favorite game genres?</div>
-//                 </div>
-//                 <div className='submit-container'>
-//                     <div
-//                         className='submit'
-//                         onClick={(e) => {
-//                             handleSubmit(e);
-//                         }}>
-//                         Create profile
-//                     </div>
-//                 </div>
-//             </div>
-
-//         </div>
-//     )
-// }
 
 export default Profile;
