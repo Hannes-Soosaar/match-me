4c1a85108d18a9f8738dfa786d23d15a26897500
markokarilaid <mkarilaid@gmail.com>
2024-12-23 07:25:51 +0000

    Update 'backend/auth/jwt.go'

--- backend/auth/jwt.go
+++ backend/auth/jwt.go
@@ -10,7 +10,7 @@ import (
 
 var jwtSecret = os.Getenv("JWT_SECRET")
 
-func GenerateJWT(userID int) (string, error) {
+func GenerateJWT(userID string) (string, error) {
 	claims := jwt.MapClaims{
 		"sub": userID,
 		"exp": time.Now().Add(time.Hour * 24).Unix(),
