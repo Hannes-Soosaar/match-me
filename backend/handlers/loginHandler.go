20a15de59162e6d7f20bd4ea9e25c3506b7524b9
markokarilaid <mkarilaid@gmail.com>
2024-12-23 07:13:56 +0000

    Update 'backend/handlers/loginHandler.go'

--- backend/handlers/loginHandler.go
+++ backend/handlers/loginHandler.go
@@ -2,49 +2,58 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
+	"log"
 	"match_me_backend/auth"
 	"match_me_backend/db"
+	"match_me_backend/models"
 	"net/http"
-	"os"
-	"time"
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	var user RegLoginUser
 
 	err := json.NewDecoder(r.Body).Decode(&user)
-
 	if err != nil {
 		sendErrorResponse(w, "Invalid request payload", http.StatusBadRequest)
+		log.Printf("Invalid login request: %v", err)
 		return
 	}
 
-	if user.Password == "" || user.Email == "" {
-		sendErrorResponse(w, "Email and password are required", http.StatusBadRequest)
+	if user.Password == "" || (user.Email == "" && user.Username == "") {
+		sendErrorResponse(w, "Password and either Email or Username are required", http.StatusBadRequest)
+		log.Println("Password and either Email or Username are required")
 		return
 	}
 
-	existingUser, err := db.GetUserByEmail(user.Email)
-	time.Sleep(500 * time.Millisecond)
-	if err != nil && err.Error() != "no user found with that email" {
+	var existingUser *models.User
+	if user.Username != "" {
+		existingUser, err = db.GetUserByUsername(user.Username)
+	} else {
+		existingUser, err = db.GetUserByEmail(user.Email)
+	}
+
+	if err != nil {
+		if errors.Is(err, db.ErrUserNotFound) {
+			sendErrorResponse(w, "Email or password is incorrect", http.StatusUnauthorized)
+			log.Println("Email or password is incorrect")
+			return
+		}
 		sendErrorResponse(w, "Error checking user existence", http.StatusInternalServerError)
+		log.Printf("Error checking user existence: %v", err)
 		return
 	}
 
 	if existingUser == nil || !auth.ComparePasswords(existingUser.Password, user.Password) {
 		sendErrorResponse(w, "Email or password is incorrect", http.StatusUnauthorized)
-		return
-	}
-
-	var jwtSecret = os.Getenv("JWT_SECRET")
-	if jwtSecret == "" {
-		sendErrorResponse(w, "JWT_SECRET not set in environment", http.StatusInternalServerError)
+		log.Println("Email or password is incorrect")
 		return
 	}
 
 	token, err := auth.GenerateJWT(existingUser.ID)
 	if err != nil {
 		sendErrorResponse(w, "Error generating JWT", http.StatusInternalServerError)
+		log.Printf("Error generating JWT: %v", err)
 		return
 	}
 
