c511d4a156ac8688b76cc97c659b784f473bef00
hannes <hannes.study@gmail.com>
2024-12-26 21:43:09 +0200

    WIP FE test display fix

--- frontend/src/components/Matches/Matches.jsx
+++ frontend/src/components/Matches/Matches.jsx
@@ -25,7 +25,7 @@ const Matches = () => {
             <div style={{ textAlign: 'center' }}>
                 <h1>This is the page with the Matches</h1>
                 {data.map((item, index) =>
-                (<p key={index}>{Object.values(item)}</p>
+                (<p key={index}>{index+1} : {Object.values(item)[1]}</p>
                 ))}
             </div>
         </>
